1b11094c69cb43a59ecb31d0af900cb646b62695
Marco Rago <marcorago@mac-pro-di-marco.local>
2015-11-10 14:34:20 +0100

    Initial Commit

---
+++ ToDoListCore/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ToDoListCore
+//
+//  Created by Marco Rago on 10/11/15.
+//  Copyright © 2015 marcorfree. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
