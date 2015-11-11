f9167e49ed3ab82d3f932264f32f66b5e88c5b35
Marco Rago <marcorfree@gmail.com>
2015-11-11 13:17:30 +0100

    Delete

--- ToDoListCore/ViewController.swift
+++ ToDoListCore/ViewController.swift
@@ -7,12 +7,148 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet var TextField1: UITextField!
+    
+    @IBOutlet var TableView1: UITableView!
+    
+    var TDL = [String]()
+    
+    
+    
+    
+   func loadCoreData() {
+    
+    let appDel1:AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+    let context1:NSManagedObjectContext = appDel1.managedObjectContext
+    
+    
+    let request = NSFetchRequest(entityName: "Activities")
+    do {
+        let results = try context1.executeFetchRequest(request)
+
+        if !results.isEmpty {
+        
+        for item in results as! [NSManagedObject] {
+            let activity = item.valueForKey("activity") as! String
+            TDL.append( activity )
+            
+        }
+        TableView1.reloadData()
+
+        }
+    } catch {
+        print("There was an error in loading data")
+    }
+    
+    }
+
+    
+    func deleteAllCoreData() {
+        
+        let appDel1:AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        let context1:NSManagedObjectContext = appDel1.managedObjectContext
+        
+        let fetchRequest = NSFetchRequest(entityName: "Activities")
+        do {
+            let fetchedEntities = try context1.executeFetchRequest(fetchRequest) as! [NSManagedObject]
+            
+            for item in fetchedEntities  {
+                context1.deleteObject(item)
+            }
+        } catch {
+             print("There was an error in deleting data")
+        }
+        
+        do {
+            try context1.save()
+        } catch {
+            print("There was an error in deleting data")
+        }
+
+    }
+    
+    
+    func addCoreData(newvalue: AnyObject) {
+
+        let appDel1:AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        let context1:NSManagedObjectContext = appDel1.managedObjectContext
+        let newActivity = NSEntityDescription.insertNewObjectForEntityForName("Activities", inManagedObjectContext: context1)
+        newActivity.setValue(newvalue, forKey: "activity")
+        
+        do{
+        try context1.save()
+        }
+        catch{
+        print("There was an error in adding data")
+        }
+        
+    }
+    
+    
+    func deleteCoreData() {
+        
+        let appDel1:AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        let context1:NSManagedObjectContext = appDel1.managedObjectContext
+
+        
+        let predicate = NSPredicate(format: "objectID == %@", objectIDFromNSManagedObject)
+        let fetchRequest = NSFetchRequest(entityName: "Activities")
+        fetchRequest.predicate = predicate
+    
+        
+        do {
+            let fetchedEntities = try context1.executeFetchRequest(fetchRequest) as! [NSManagedObject]
+            if let entityToDelete = fetchedEntities.first {
+                context1.deleteObject(entityToDelete)
+            }
+        } catch {
+            print("There was an error in deleting data (1/2)")
+        }
+        
+        do {
+            try context1.save()
+        } catch {
+            print("There was an error in deleting data (2/2)")
+        }
+
+        
+        
+        
+        
+        
+        
+        let fetchRequest = NSFetchRequest(entityName: "Activities")
+        fetchRequest.includesSubentities = false
+        fetchRequest.returnsObjectsAsFaults = false
+        
+        fetchRequest.predicate = NSPredicate(format:"name == '\(itemName)'")
+        
+        var error: NSError?
+        
+        // moc is your NSManagedObjectContext here
+        items = moc.executeFetchRequest(fetchRequest, error: &error)!
+        
+        for item in items {
+            moc.deleteObject(item)
+        }
+        
+        
+        
+        
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        loadCoreData()
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +156,44 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func Button1(sender: UIButton) {
+        if TextField1.text != "" {
+            TDL.append(TextField1.text!)
+            TableView1.reloadData()
+            addCoreData(TextField1.text!)
+            TextField1.text = ""
+        
+        }
+    }
 
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return TDL.count
+    }
+    
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cella = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
+        
+        cella.textLabel?.text = TDL[indexPath.row]
+        
+        return cella
+    }
+    
+    
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        
+        if editingStyle == UITableViewCellEditingStyle.Delete {
+            TDL.removeAtIndex(indexPath.row)
+            TableView1.deleteRowsAtIndexPaths([indexPath], withRowAnimation: UITableViewRowAnimation.Fade)
+            
+        }
+    }
+    
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+    }
+    
 }
 
